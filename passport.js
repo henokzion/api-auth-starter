b19a6b67b24a4985c076ad1a67854be64e200176
Henok Teklu <henokzion@gmail.com>
2019-03-14 17:09:22 +0300

    bcrypt added

--- passport.js
+++ passport.js
@@ -1,24 +1,53 @@
 const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
-const { ExtractJwt } = require("passport-jwt");
+const {
+    ExtractJwt
+} = require("passport-jwt");
 
 const User = require("./models/users");
 
 const JWT_SECRET = "this is a secret";
 
-passport.use( new JwtStrategy({
-    jwtFromRequest : ExtractJwt.fromHeader("authorization"),
-    secretOrKey : JWT_SECRET
-}, async(payload, done) => {
+passport.use(new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+    secretOrKey: JWT_SECRET
+}, async (payload, done) => {
     try {
         const user = await User.findById(payload.sub);
 
-        if(!user){
+        if (!user) {
             return done(null, false);
         }
 
         done(null, user);
     } catch (error) {
         done(error, false)
+    }
+}))
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, async (email, password, done) => {
+    try {
+        // Find the user given the email
+        const user = await User.findOne({
+            "local.email": email
+        });
+
+        
+        if (!user) {
+            return done(null, false);
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        
+        if (!isMatch) {
+            return done(null, false);
+        }
+
+        done(null, user);
+    } catch (error) {
+        done(error, false);
     }
 }))
