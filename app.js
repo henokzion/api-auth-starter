45c594f5ca359c7efd2855aef97019d026fb4def
Henok Teklu <henokzion@gmail.com>
2019-03-21 19:15:07 +0300

    cors added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 mongoose.connect("mongodb://localhost/LearningIDE", { useNewUrlParser: true });
 
