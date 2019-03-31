33e962a270908c1496451c6a2eb27a6c9e0a9da9
Henok Teklu <henokzion@gmail.com>
2019-03-31 21:16:07 +0300

    try catch on sendgrid

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ const signToken = (user) => {
 var sendVerifyEmail = (res, user) => {
     const token = signToken(user);
     var verificationText = `http://localhost:3000/confirmation?verify=${token}`;
-    
+
     sgMail.setApiKey("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");
     const msg = {
         to: user.local.email,
@@ -27,12 +27,14 @@ var sendVerifyEmail = (res, user) => {
         text: verificationText
     };
     sgMail.send(msg);
-    res.status(200).json({"message": "not verified"});
+    res.status(200).json({
+        "message": "not verified"
+    });
 }
 
 const signUp = async (req, res) => {
     const foundUser = await User.findOne({
-        "local.email" : req.body.email
+        "local.email": req.body.email
     });
     if (foundUser) {
         return res.status(403).json({
@@ -41,7 +43,7 @@ const signUp = async (req, res) => {
     }
     console.log(foundUser)
     const user = new User({
-        username : req.body.username,
+        username: req.body.username,
         local: {
             email: req.body.email,
             password: req.body.password
@@ -75,28 +77,30 @@ const googleOAuth = async (req, res, next) => {
 }
 
 
-var getLinkedinInfo = (res, access_token)=>{
+var getLinkedinInfo = (res, access_token) => {
     //https://api.linkedin.com/v2/me
     request({
         headers: {
             'Authorization': `Bearer ${access_token}`
         },
-        uri : "https://api.linkedin.com/v2/people/~:(id,email-address?format=json",
-        method : "GET"
-    }, (err, result, body)=>{
-        if(err){
+        uri: "https://api.linkedin.com/v2/people/~:(id,email-address?format=json",
+        method: "GET"
+    }, (err, result, body) => {
+        if (err) {
             console.log(err);
             res.json(err);
         }
-        body=JSON.parse(body);
+        body = JSON.parse(body);
         console.log(body);
         saveLinkedinInfo(res, body);
     })
 }
 
-var saveLinkedinInfo = async (res, profile)=>{
-    const existingUser = await User.findOne({ "linkedin.id": profile.id });
-    if(existingUser){
+var saveLinkedinInfo = async (res, profile) => {
+    const existingUser = await User.findOne({
+        "linkedin.id": profile.id
+    });
+    if (existingUser) {
         const token = signToken(existingUser);
         return res.status(200).json({
             token
@@ -106,23 +110,25 @@ var saveLinkedinInfo = async (res, profile)=>{
     const newUser = new User({
         method: "linkedin",
         linkedin: {
-            id : profile.id ,
-            email : profile.emailAddress
+            id: profile.id,
+            email: profile.emailAddress
         },
-       
+
     });
 
     await newUser.save();
 
     token = signToken(newUser);
-    res.status(200).json({token});
+    res.status(200).json({
+        token
+    });
 
 }
 
 const signInWithLinkedin = async (req, res, next) => {
     var data = {
         grant_type: "authorization_code",
-        code : req.body.code,
+        code: req.body.code,
         redirect_uri: "http://localhost:3000/linkedin",
         client_id: "77km4hwlu5w8xt",
         client_secret: "Vo23lvnW6Cmy29x8"
@@ -130,62 +136,74 @@ const signInWithLinkedin = async (req, res, next) => {
     var formData = querystring.stringify(data);
     var contentLength = formData.length;
     console.log(formData);
-    
+
     request({
         headers: {
-          'Content-Length': contentLength,
-          'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
         uri: 'https://www.linkedin.com/oauth/v2/accessToken',
         body: formData,
         method: 'POST'
-      }, function (err, result, body) {
+    }, function (err, result, body) {
         //it works!
-        if(err){
-                console.log(err);
-                res.json(error);
-            }
-            console.log(JSON.parse(body).access_token);
-            access_token = JSON.parse(body).access_token;
-            getLinkedinInfo(res, access_token);
-      });
+        if (err) {
+            console.log(err);
+            res.json(error);
+        }
+        console.log(JSON.parse(body).access_token);
+        access_token = JSON.parse(body).access_token;
+        getLinkedinInfo(res, access_token);
+    });
 }
 var sendPasswordChangeEmail = async (res, user) => {
-    const token = signToken(user);
+    try {
+        const token = signToken(user);
+        var verificationText = `http://localhost:3000/changepasswordverify?verify=${token}`;
 
-    //"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"
-    var verificationText = `http://localhost:3000/changepasswordverify?verify=${token}`;
-    
-    sgMail.setApiKey("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");
-    const msg = {
-        to: user.local.email,
-        from: 'noreply@briter.com',
-        subject: 'Change Your password',
-        text: verificationText
-    };
-    sgMail.send(msg);
-    res.status(200).json({"message": "not verified"});
+        sgMail.setApiKey("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");
+        const msg = {
+            to: user.local.email,
+            from: 'noreply@briter.com',
+            subject: 'Change Your password',
+            text: verificationText
+        };
+        sgMail.send(msg);
+        res.status(200).json({
+            "message": "not verified"
+        });
+    } catch (error) {
+        res.json(error)
+    }
+
 }
 
-const requestPasswordChange = async (req, res)=>{
-    if(req.body.email){
+const requestPasswordChange = async (req, res) => {
+    if (req.body.email) {
         User
-            .findOne({method : 'local', "local.email" : req.body.email})
-            .exec((err, user)=>{
-                if(err)
+            .findOne({
+                method: 'local',
+                "local.email": req.body.email
+            })
+            .exec((err, user) => {
+                if (err)
                     return res.status(400).json(err);
-                if(user)
+                if (user)
                     sendPasswordChangeEmail(res, user);
                 else
-                    res.status(404).json({"message":"user not found"});
+                    res.status(404).json({
+                        "message": "user not found"
+                    });
             })
-    
-    }else{
-        res.json({"message": "please provide a valid email"});
+
+    } else {
+        res.json({
+            "message": "please provide a valid email"
+        });
     }
 }
 
-verify = async(req, res, next)=>{
+verify = async (req, res, next) => {
     const token = signToken(req.user);
     res.status(200).json({
         token
