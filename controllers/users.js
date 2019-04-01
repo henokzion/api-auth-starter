1f4f6badc278a5e6c7ecca509449402cc9695eb9
Henok Teklu <henokzion@gmail.com>
2019-04-01 12:42:27 +0300

    send grid api key changed

--- controllers/users.js
+++ controllers/users.js
@@ -18,8 +18,7 @@ const signToken = (user) => {
 var sendVerifyEmail = (res, user) => {
     const token = signToken(user);
     var verificationText = `http://localhost:3000/confirmation?verify=${token}`;
-
-    sgMail.setApiKey("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");
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
         to: user.local.email,
         from: 'noreply@briter.com',
@@ -157,18 +156,19 @@ const signInWithLinkedin = async (req, res, next) => {
     });
 }
 var sendPasswordChangeEmail = async (res, user) => {
+        console.log(process.env.SENDGRID_API_KEY)
     try {
         const token = signToken(user);
         var verificationText = `http://localhost:3000/changepasswordverify?verify=${token}`;
-
-        sgMail.setApiKey("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");
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
             to: user.local.email,
             from: 'noreply@briter.com',
             subject: 'Change Your password',
             text: verificationText
         };
-        sgMail.send(msg);
+        const mail = sgMail.send(msg);
+        console.log(mail);
         res.status(200).json({
             "message": "not verified"
         });
